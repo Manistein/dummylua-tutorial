f1ff777f0c82026a503dceb2f91def7fec756560
manistein <1518986465@qq.com>
2019-09-19 20:05:12 +0800

    add luaM_growvector and luaM_growaux_ function

--- windows/project/part05/common/luamem.c
+++ windows/project/part05/common/luamem.c
@@ -21,6 +21,29 @@ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 #include "luamem.h"
 #include "../vm/luado.h"
 
+#define MINARRAYSIZE 4
+
+void* luaM_growaux_(struct lua_State* L, void* ptr, int* size, int element, int limit)
+{
+	void* newblock = NULL;
+	int block_size = *size;
+	if (block_size > limit / 2) {
+		if (block_size > limit) {
+			LUA_ERROR(L, "luaM_growaux_ size too big");
+			luaD_throw(L, LUA_ERRMEM);
+		}
+
+		block_size = limit;
+	}
+	else {
+		block_size *= 2;
+	}
+
+	newblock = luaM_realloc(L, ptr, *size * element, block_size * element);
+	*size = block_size;
+	return newblock;
+}
+
 void* luaM_realloc(struct lua_State* L, void* ptr, size_t osize, size_t nsize) {
     struct global_State* g = G(L);
     int oldsize = ptr ? osize : 0;
