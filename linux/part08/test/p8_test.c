8a3e8d13d85f41bc55b5ed07f9330cfc9c49a323
manistein <1518986465@qq.com>
2020-11-11 21:19:35 +0800

    update

--- linux/part08/test/p8_test.c
+++ linux/part08/test/p8_test.c
@@ -5,7 +5,7 @@ void p8_test_main() {
 	struct lua_State* L = luaL_newstate();
 	luaL_openlibs(L);
 
-	int ok = luaL_loadfile(L, "../part08/scripts/part08_test.lua");
+	int ok = luaL_loadfile(L, "../dummylua/scripts/part08_test.lua");
 	if (ok == LUA_OK) {
 		luaL_pcall(L, 0, 0);
 	}
